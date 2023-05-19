e3d39295091e66ad62425bb734ab843feab527d0
SamuelBenicio <133923771+SamuelBenicio@users.noreply.github.com>
2023-05-19 08:53:11 -0300

    Problema 1174 do beecrowd resolvido em python

---
+++ 1174.py
@@ -0,0 +1,9 @@
+vetor = []
+for c in range(100):
+    n1 = float(input())
+    vetor.append(n1)
+for c in range(100):
+    if(vetor[c]<=10):
+        print(f'A[{c}] = {vetor[c]:.1f}')
+    else:
+        pass
